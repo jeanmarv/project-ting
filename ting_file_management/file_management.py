39bbe0e556db1514fb96fbc255228cae9b3cb9f6
Jean Marcel <jeanvanzella91@gmail.com>
2022-09-27 15:59:21 -0300

    req 2, txt importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if '.txt' not in path_file:
+            return sys.stderr.write("Formato inválido\n")
+        with open(path_file) as file:
+            file_open = file.read().splitlines()
+            return file_open
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
