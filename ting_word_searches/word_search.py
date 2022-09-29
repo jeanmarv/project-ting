9e1118f0dbc308575169722541490305ea927f1c
Jean Marcel <jeanvanzella91@gmail.com>
2022-09-29 16:11:59 -0300

    req 6-7, search by a existing word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,38 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrence = []
+    all_files = []
+    for index in range(len(instance)):
+        searched = instance.search(index)
+        for index2, itens in enumerate(searched["linhas_do_arquivo"]):
+            if word.lower() in itens.lower():
+                occurrence.append({"linha": index2 + 1})
+            else:
+                all_files.append({
+                    "arquivo": searched["nome_do_arquivo"],
+                    "ocorrencias": occurrence,
+                    "palavra": word,
+                })
+    if len(occurrence) == 0:
+        return []
+    else:
+        return all_files
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrence = []
+    all_files = []
+    for index in range(len(instance)):
+        searched = instance.search(index)
+        for index2, itens in enumerate(searched["linhas_do_arquivo"]):
+            if word.lower() in itens.lower():
+                occurrence.append({"linha": index2 + 1, "conteudo": itens})
+            else:
+                all_files.append({
+                    "arquivo": searched["nome_do_arquivo"],
+                    "ocorrencias": occurrence,
+                    "palavra": word,
+                })
+    if len(occurrence) == 0:
+        return []
+    else:
+        return all_files
